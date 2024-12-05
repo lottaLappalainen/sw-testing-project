3ca87f28225a0ba6b85075030b3d528fba9b6077
Lotta Lappalainen <lotta.lappalainen@tuni.fi>
2024-12-05 17:29:47 +0200

    test coveralls

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,9 @@ export default {
     coverageDirectory: "coverage",
     coverageReporters: ["lcov", "text"],
     collectCoverageFrom: [
-        "src/add.js",
-        "src/ceil.js",
-        "src/eq.js",
-        "src/every.js",
-        "src/filter.js",
-        "src/get.js",
-        "src/isEmpty.js",
+       
         "src/map.js",
-        "src/reduce.js",
-        "src/toNumber.js"
+      
       ],
     coveragePathIgnorePatterns: ["src/.internal/"]
   };
